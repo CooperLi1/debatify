efe78790130326ee4fbd37d9ec8faf642c9825c4
CooperLi1 <112364250+CooperLi1@users.noreply.github.com>
2025-05-26 02:27:21 -0400

    update

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY_LIVE ?? process.env.STRIPE_SECRET_KEY ?? '',
   {
-    apiVersion: '2025-03-31.basil',
+    apiVersion: '2025-04-30.basil',
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
